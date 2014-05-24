b3b806894cabc4f55b75b0ec791909698d2fe9e8
Zoran Plesivčak <zplesivcak@gmail.com>
2014-05-24 19:36:48 +0200

    Logging module - initial

---
+++ logging.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"""
+Logging module.
+"""
+
+from os.path import join, abspath
+import datetime
+
+def logg(*st):
+    with open("/tmp/ranger.log", 'a') as myfile:
+        ban1 = ban2  = ('-> FILE: ' + join(abspath('.'),__file__)).ljust(60)
+        ban1 += ('   PCKG: ' + str(__package__)).ljust(25)
+        ban2 += ''.ljust(25)
+        for i,cs in enumerate(st):
+            myfile.write((ban1 if i==0 else ban2) + str(cs) + '\n')
+
+def header(*st):
+    with open("/tmp/ranger.log", 'a') as myfile:
+        myfile.write('\n\n' + '-'*30 + '\n' + str(datetime.datetime.now())
+            + '\n' + '-'*30 + '\n')
+
+def logg_modules():
+    logg('[ MODULES ]', *[ a for (a,b) in globals().items() if 'module' in str(b) ])
+
+def logg_sys_path():
+    logg('[ sys.path ]', *sys.path)
+
+def logg_dir():
+    logg('[ dir() ]', *dir())
+
