923049571bf25d7b24090ae52d5986f6af61cfa2
Zoran Plesivčak <zplesivcak@gmail.com>
2014-05-24 19:52:49 +0200

    Logging module - configuration variables

--- logging.py
+++ logging.py
@@ -7,16 +7,21 @@ Logging module.
 from os.path import join, abspath
 import datetime
 
+log_file = '/tmp/ranger.log'
+
+cur_file = '<Not given>'
+cur_pckg = '<Not given>'
+
 def logg(*st):
     with open("/tmp/ranger.log", 'a') as myfile:
-        ban1 = ban2  = ('-> FILE: ' + join(abspath('.'),__file__)).ljust(60)
-        ban1 += ('   PCKG: ' + str(__package__)).ljust(25)
+        ban1 = ban2  = ('-FILE: ' + cur_file).ljust(60)
+        ban1 += ('   PCKG: ' + cur_pckg).ljust(25)
         ban2 += ''.ljust(25)
         for i,cs in enumerate(st):
             myfile.write((ban1 if i==0 else ban2) + str(cs) + '\n')
 
 def header(*st):
-    with open("/tmp/ranger.log", 'a') as myfile:
+    with open(log_file, 'a') as myfile:
         myfile.write('\n\n' + '-'*30 + '\n' + str(datetime.datetime.now())
             + '\n' + '-'*30 + '\n')
 
